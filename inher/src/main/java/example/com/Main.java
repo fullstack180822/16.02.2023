591ae2c3fc0eddee7fb4ae93260d2407e57133d7
micserv010821 <dotnet290320@gmail.com>
2023-02-16 20:39:52 +0200

    Update Main.java

--- inher/src/main/java/example/com/Main.java
+++ inher/src/main/java/example/com/Main.java
@@ -15,6 +15,19 @@ public class Main {
 
         System.out.println(rex);
 
+        // create a base class Phone
+        // fields: private  size, color
+        // @ Getter/ @ Setter
+        // ctor with all args (+lombok?)
+        // toString prints all fields
+        // create a sub class Android
+        // fields: private osVersion
+        // @ Getter/ @ Setter
+        // toString which prints osVersion + super to-string
+        // ctor which gets size, color, osVersion
+        // ctor which gets size, osVersion (default color will be Black)
+        // ctor which gets osVersion (default size will be 6.5f and will call the other ctor)
+
     }
 
 }
